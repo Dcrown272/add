dddb3050ae5a9d40653c035d23c481f02ada5937
Dcrown272 <thanhpham2722001@gmail.com>
2022-02-15 01:28:16 +0700

    first commit

---
+++ app/src/main/java/com/android/attendance/activity/Profile.java
@@ -0,0 +1,15 @@
+package com.android.attendance.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.androidattendancesystem.R;
+
+public class Profile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
